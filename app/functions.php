c3a60f97a969baf9931c9c9475b6c84a7a3e2573
Marcus Schwemer <marcus.schwemer@in2code.de>
2019-10-25 13:58:13 +0200

    [FEATURE] Add typehints to all functions

--- app/functions.php
+++ app/functions.php
@@ -1,14 +1,14 @@
 <?php
 
 if (!function_exists('array_column_index')) {
-    function array_index_by_column(array $array, string $index)
+    function array_index_by_column(array $array, string $index): array
     {
         return array_combine(array_column($array, $index), $array);
     }
 }
 
 if (!function_exists('array_filter_implode')) {
-    function array_filter_implode(array $array, string $glue = '')
+    function array_filter_implode(array $array, string $glue = ''): string
     {
         return implode($glue, array_filter($array));
     }
